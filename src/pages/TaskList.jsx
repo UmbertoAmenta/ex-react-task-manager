cf5d66ce3ae8e81a7ceca2ebda333845d531b1a8
Umberto Amenta <umberto.amenta@hotmail.it>
2025-06-13 20:35:42 +0200

    feat: milestone 11 (ordinamento delle task)

--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useMemo, useState } from "react";
 import TasksContext from "../contexts/TasksContext";
 
 // components
@@ -6,15 +6,77 @@ import TaskRow from "../components/TaskRow";
 
 export default function TaskList() {
   const { tasks } = useContext(TasksContext);
+
+  const [sortBy, setSortBy] = useState("createdAt");
+  const [sortOrder, setSortOrder] = useState(1);
+
+  const sortedTasks = useMemo(() => {
+    const statusOrder = { "To do": 0, Doing: 1, Done: 2 };
+
+    return [...tasks].sort((a, b) => {
+      if (sortBy === "title") {
+        return a.title.localeCompare(b.title) * sortOrder;
+      }
+
+      if (sortBy === "status") {
+        return (statusOrder[a.status] - statusOrder[b.status]) * sortOrder;
+      }
+
+      if (sortBy === "createdAt") {
+        return (
+          (new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()) *
+          sortOrder
+        );
+      }
+      return 0;
+    });
+  }, [tasks, sortBy, sortOrder]);
+
   return (
     <main className="container">
       <div>
         <div className="grid head">
-          <div>Attività</div>
-          <div>Status</div>
-          <div>Data di creazione</div>
+          <button
+            onClick={() => {
+              if (sortBy === "title") {
+                setSortOrder(sortOrder * -1);
+              } else {
+                setSortBy("title");
+                setSortOrder(1);
+              }
+            }}
+          >
+            Attività
+            {sortBy === "title" ? (sortOrder === 1 ? "⬆️" : "⬇️") : null}
+          </button>
+          <button
+            onClick={() => {
+              if (sortBy === "status") {
+                setSortOrder(sortOrder * -1);
+              } else {
+                setSortBy("status");
+                setSortOrder(1);
+              }
+            }}
+          >
+            Status
+            {sortBy === "status" ? (sortOrder === 1 ? "⬆️" : "⬇️") : null}
+          </button>
+          <button
+            onClick={() => {
+              if (sortBy === "createdAt") {
+                setSortOrder(sortOrder * -1);
+              } else {
+                setSortBy("createdAt");
+                setSortOrder(1);
+              }
+            }}
+          >
+            Data di creazione
+            {sortBy === "createdAt" ? (sortOrder === 1 ? "⬆️" : "⬇️") : null}
+          </button>
         </div>
-        {tasks.map((t) => (
+        {sortedTasks.map((t) => (
           <TaskRow key={t.id} task={t} />
         ))}
       </div>
