d6f123504303cbecb8f7698d34ce66e30d9d9e8f
Umberto Amenta <umberto.amenta@hotmail.it>
2025-06-14 12:06:06 +0200

    feat: milestone 12 (ricerca dei task con debounce)

--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,19 +1,36 @@
-import { useContext, useMemo, useState } from "react";
+import { useCallback, useContext, useMemo, useState } from "react";
 import TasksContext from "../contexts/TasksContext";
 
 // components
 import TaskRow from "../components/TaskRow";
 
+import debounce from "../utilities/debounce";
+
 export default function TaskList() {
   const { tasks } = useContext(TasksContext);
+
+  const [searchQuery, setSearchQuery] = useState("");
 
   const [sortBy, setSortBy] = useState("createdAt");
   const [sortOrder, setSortOrder] = useState(1);
 
+  const handleSearch = useCallback(
+    debounce((e) => {
+      setSearchQuery(e.target.value);
+    }, 400),
+    [searchQuery]
+  );
+
+  const filteredTasks = useMemo(() => {
+    return tasks.filter((t) =>
+      t.title.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+  }, [tasks, searchQuery]);
+
   const sortedTasks = useMemo(() => {
     const statusOrder = { "To do": 0, Doing: 1, Done: 2 };
 
-    return [...tasks].sort((a, b) => {
+    return [...filteredTasks].sort((a, b) => {
       if (sortBy === "title") {
         return a.title.localeCompare(b.title) * sortOrder;
       }
@@ -30,10 +47,18 @@ export default function TaskList() {
       }
       return 0;
     });
-  }, [tasks, sortBy, sortOrder]);
+  }, [filteredTasks, sortBy, sortOrder]);
 
   return (
     <main className="container">
+      <section className="search-bar">
+        <input
+          type="text"
+          // value={searchQuery}
+          onChange={handleSearch}
+          placeholder="Digita e cerca"
+        />
+      </section>
       <div>
         <div className="grid head">
           <button
---
+++ src/utilities/debounce.jsx
@@ -0,0 +1,11 @@
+function debounce(callback, delay) {
+  let timer;
+  return (value) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      callback(value);
+    }, delay);
+  };
+}
+
+export default debounce;
